89a079977271302104c126c791d4274f1141d9ea
Silver0se13 <gurleens0013@gmail.com>
2024-12-10 14:05:03 -0700

    Created new html pages. added js links for buttons

---
+++ script.js
@@ -0,0 +1,15 @@
+home.addEventListener('click', function (){
+    window.location.href = "index.html"
+})
+
+me.addEventListener('click', function (){
+    window.location.href = "AboutMe.html"
+})
+
+socials.addEventListener('click', function (){
+    window.location.href = "socials.html"
+})
+
+comms.addEventListener('click', function (){
+    window.location.href = "commission.html"
+})
